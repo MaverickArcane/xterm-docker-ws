4440de174f0772f49ba288f209044155c94c76ec
Diego Briceno <115434468+MaverickArcane@users.noreply.github.com>
2023-11-19 18:39:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const port = process.env.FRONTEND_PORT
 app.use(express.static('public'));
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', (req, res) => {
-  res.render(__dirname + "/index.html", {port:process.env.BACKEND_PORT});
-})
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
